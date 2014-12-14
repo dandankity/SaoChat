f91cae7cddca88a88a12198475ba6df7cca15896
Jian Lv <jlv@thoughtworks.com>
2014-12-14 19:19:15 +0800

    Initial Commit

---
+++ SaoChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SaoChat
+//
+//  Created by lvjian on 12/14/14.
+//  Copyright (c) 2014 lvjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SaoChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SaoChat
+//
+//  Created by lvjian on 12/14/14.
+//  Copyright (c) 2014 lvjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
